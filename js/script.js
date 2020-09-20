ffdcaedd73bfddd6189d17473fbe1154995dc71d
Dzis <Dzis99@yandex.ru>
2020-09-20 14:27:37 +0500

    Добавление файлов проекта

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let addMessage = document.querySelector('.message');
+let addButton = document.querySelector('.add');
+let todo = document.querySelector('.todo');
+
+let todoList = [];
+
+if(localStorage.getItem('todo')) {  //Вывод значений из хранилища
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages;
+}
+
+addButton.addEventListener('click', function() {
+
+let newTodo = {
+    todo: addMessage.value,
+    checked: false,
+    important: false
+};
+
+    todoList.push(newTodo)
+    displayMessages();
+    localStorage.setItem('todo', JSON.stringify(todoList)); //Создание локальной базы
+})
+
+function displayMessages() {
+    let displayMessage = ``;
+    todoList.forEach(function(item, i){
+        displayMessage += `
+        <li>
+            <input type='checkbox' id='item_${i}' ${item.checked ?  'checked' : ''}>
+            <label for='item_${i}'>${item.todo}</label>
+        </li>
+        `
+        ;
+        todo.innerHTML = displayMessage;
+    });
+}
+
+todo.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id');
+    let valueLabel = todo.querySelector('[for='+ idInput +']').innerHTML;
+    
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    })
+});
